46ef14575c06dd5fa2ae6f78afdbd38e6c0d52ad
dnao <dnao9437@naver.com>
2023-12-18 22:31:17 +0900

    first commi

---
+++ deungchi/src/main/java/com/netj/deungchi/DeungchiApplication.java
@@ -0,0 +1,13 @@
+package com.netj.deungchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeungchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeungchiApplication.class, args);
+	}
+
+}
---
+++ deungchi/src/test/java/com/netj/deungchi/DeungchiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netj.deungchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeungchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
