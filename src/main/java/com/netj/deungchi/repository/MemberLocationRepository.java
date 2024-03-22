83ac0f1b2f3b42cf340b2c298e7f660bf96954dd
haenoya <hoyaa@bigc.im>
2024-03-23 06:06:28 +0900

    등산 중 이동거리 계산해서 프론트 전달

--- src/main/java/com/netj/deungchi/repository/MemberLocationRepository.java
+++ src/main/java/com/netj/deungchi/repository/MemberLocationRepository.java
@@ -3,5 +3,8 @@ package com.netj.deungchi.repository;
 import com.netj.deungchi.domain.MemberLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberLocationRepository extends JpaRepository<MemberLocation, Long> {
+    List<MemberLocation> findAllByMemberIdAndRecordIdOrderByIdAsc(Long memberId, Long recordId);
 }
